4b44740161b901c3b0be31884e7c45895f4b004d
Hpaila <58894262+Hpaila@users.noreply.github.com>
2020-10-26 12:45:55 -0700

    Added Main as Main77 - Harshita

---
+++ src/Main77.java
@@ -0,0 +1,67 @@
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * @author Harshita Paila
+ * @since 10/26/2020
+ */
+public class Main77 extends JFrame {
+
+    /***
+     * Creates the frame, file chooser and necessary buttons to generate reports
+     */
+    public void createFrame(){
+        AtomicReference<StudentRepository> studentRepository = null;
+        Grader73 grader = new Grader73();
+        Reporter reporter = new Reporter();
+
+        JButton openRoster = new JButton("Open Roster");
+        JButton openGrades = new JButton("Open Grades");
+        JButton openAttendance = new JButton("Open Attendance");
+        openRoster.addActionListener(e -> studentRepository.set(new StudentRepository(openFile())));
+        openGrades.addActionListener(e -> grader.init(openFile()));
+        openAttendance.addActionListener(e -> reporter.studentAttendance(openFile()));
+
+        StudentSubject studentSubject = new StudentSubject();
+        GraphObserver graphObserver = new GraphObserver();
+        studentSubject.register(graphObserver);
+        add(graphObserver);
+        TableObserver tableObserver = new TableObserver();
+        studentSubject.register(tableObserver);
+        add(tableObserver);
+
+        JPanel topPanel = new JPanel();
+        topPanel.add(openRoster);
+        topPanel.add(openGrades);
+        topPanel.add(openAttendance);
+        add(topPanel, BorderLayout.NORTH);
+        setExtendedState(getExtendedState() | MAXIMIZED_BOTH);
+
+        setVisible(true);
+    }
+
+    /***
+     * File opener to get path of selected file
+     * @return path of file as String
+     */
+    public static String openFile(){
+        JFileChooser fileChooser = new JFileChooser(System.getProperty("user.dir"));
+        int returnValue = fileChooser.showOpenDialog(null);
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = fileChooser.getSelectedFile();
+            return selectedFile.getPath();
+        }
+        return null;
+    }
+
+    /***
+     * Main class to begin the application
+     * @param args null
+     */
+    public static void main(String[] args) {
+        Main77 main = new Main77();
+        main.createFrame();
+    }
+}
